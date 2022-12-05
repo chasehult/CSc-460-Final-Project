6e93ad8a6646f0453bf41c1228216168ec9d61be
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2022-12-04 22:59:17 -0700

    query one done

--- Controller.java
+++ Controller.java
@@ -1,2 +1,51 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Controller{
+	
+	public static void executeQueryOne(Connection con, String prefix) {
+		try {
+			Statement stmt = con.createStatement();	// The statement to execute the query
+			ResultSet answer = null; // The answer to the query
+			
+			// The query to execute to oracle
+			String query = "SELECT DISTINCT name "
+					+ "FROM " + prefix + ".Passenger "
+							+ "JOIN " + prefix + ".PassengerFlight USING (passenger_id) "
+							+ "JOIN " + prefix + ".Flight USING (flight_id) "
+					+ "GROUP BY passenger_id, name "
+					+ "HAVING COUNT(DISTING airline) = 4";
+			
+			answer = stmt.executeQuery(query);
+
+            if (answer != null) {
+
+                System.out.println("\nThe results of the query are:\n");
+
+                    // Get the data about the query result to learn
+                    // the attribute names and use them as column headers
+
+                ResultSetMetaData answermetadata = answer.getMetaData();
+
+                for (int i = 1; i <= answermetadata.getColumnCount(); i++) {
+                    System.out.print(answermetadata.getColumnName(i) + "\t");
+                }
+                System.out.println("\n-------------");
+
+                    // Use next() to advance cursor through the result
+                    // tuples and print their attribute values
+
+                while (answer.next()) System.out.println(answer.getString(1));
+            }
+            	System.out.println("=============\n");
+            
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
